a5611c18a83e8286ca1af40d04c33f7b60078ae9
Thomas Delmotte <marcodlmt@gmail.com>
2026-02-10 16:37:45 +0100

    feat add context and api

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,102 @@
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useState,
+  useContext,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { api } from "../services/api";
+
+interface UserType {
+  id: number;
+  email: string;
+}
+
+interface AuthContextType {
+  user: UserType | null;
+  token: string | null;
+  isLoading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  register: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<UserType | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    loadStoreAuth();
+  }, []);
+
+  const loadStoreAuth = async () => {
+    try {
+      const storedToken = await AsyncStorage.getItem("token");
+      const storedUser = await AsyncStorage.getItem("user");
+      if (storedToken && storedUser) {
+        api.setToken(storedToken);
+        setToken(storedToken);
+        setUser(JSON.parse(storedUser));
+      }
+    } catch (error) {
+      console.log("une erruer chargement auth", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    const response = await api.login(email, password);
+
+    await AsyncStorage.setItem("token", response.token);
+    await AsyncStorage.setItem("token", JSON.stringify(response.user));
+    api.setToken(response.token);
+    setToken(response.token);
+    setUser(response.user);
+  };
+
+  const register = async (email: string, password: string) => {
+    const response = await api.register(email, password);
+
+    await AsyncStorage.setItem("token", response.token);
+    await AsyncStorage.setItem("token", JSON.stringify(response.user));
+    api.setToken(response.token);
+    setToken(response.token);
+    setUser(response.user);
+  };
+
+  const logout = async () => {
+    await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("user");
+    api.setToken(null);
+    setToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        token,
+        isLoading,
+        login,
+        register,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth doit etre dans un AuthProviders");
+  }
+  return context;
+}
---
+++ services/api.ts
@@ -0,0 +1,73 @@
+const BASE_URL = "http://localhost:3000/api";
+
+class ApiService {
+  private token: string | null = null;
+
+  setToken(token: string | null) {
+    this.token = token;
+  }
+
+  private async request<T>(
+    endpoint: string,
+    options: RequestInit = {},
+  ): Promise<T> {
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+      ...options.headers,
+    };
+
+    if (this.token) {
+      (headers as Record<string, string>)["Authorization"] =
+        `Bearer ${this.token}`;
+    }
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+      ...options,
+      headers,
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || "Une erreur est survenue");
+    }
+
+    return data;
+  }
+  // Auth
+
+  async register(email: string, password: string) {
+    return this.request<{ token: string; user: { id: number; email: string } }>(
+      "/auth/register",
+      {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+      },
+    );
+  }
+
+  async login(email: string, password: string) {
+    return this.request<{ token: string; user: { id: number; email: string } }>(
+      "/auth/login",
+      {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+      },
+    );
+  }
+
+  // Récupération getMe
+
+  async getMe() {
+    return this.request<{
+      user: { id: number; email: string; expo_push_token: string | null };
+    }>("/auth/me");
+  }
+
+  async savePushToken(expo_push_token: string){
+    return this.request('/auth/push-token', {
+      method: 'PATCH',
+      body: JSON.stringify({expo_push_token})
+    })
+  }
+
+}
+export const api= new ApiService();
